27d6520353a30c84dcb25472c816ebf972350e47
Howard Seltman <hseltman@stat.cmu.edu>
2018-06-25 12:54:19 -0400

    Make fully functional "Problem" tabs by adding code to genProblemTabs.R.

--- genProblemTabs.R
+++ genProblemTabs.R
@@ -2,22 +2,44 @@
 # H. Seltman, June 2018
 
 # Generate code like:
+#
 # tabPanel("P1",
 #          radioButtons("codefile", label="Choose a codefile for Problem 1:",
 #            choices="(None)"),
 #          textInput("inputReq", "Input requirements", ""),
-#          textInput("inputAnath", "Input anathemas", ""),
-#          textInput("outputReq", "Output requirements", ""),
-#          textInput("outputAnath", "Output anathemas", "")
+#          textInput("inputAnath", "Input anathemas", ""))
 # )
-
+#
 # from a string vector containing the text with "##" for the problem number.
+
 
 probPanelCodeOne = c(
   '  tabPanel("Problem ##",',
-  '    checkboxGroupInput("codefiles##", label="Choose a codefile(s) for Problem ##:",',
-  '      choices="(None)"),',
-  '    textInput("inputReq##", "Input requirements", ""))'
+  '    fluidRow(column(2, selectInput("runFileType##", "Type of code to run",',
+  '                                   c("(none)", ".R", ".Rmd", ".R or .Rmd",',
+  '                                     ".py", ".sas"))),',
+  '             column(10, textInput("runFileName##", "Name (or re) of code to run"))),',
+  '    textInput("filesToCopy##",',
+  '              paste("Names (semicolon separated) or regular expressions of other",',
+  '                    "files to copy to sandbox"), width="100%"),',
+  '    textInput("filesToCopyAndDelete##",',
+  '              paste("Names (semicolon separated) or regular expressions of other",',
+  '                    "files to copy to sandbox and delete afterwards"), width="100%"),',
+  '    HTML("&nbsp"),',
+  '    checkboxInput("doPdf##", "Attempt to make pdf"),',
+  '    HTML("&nbsp"),',
+  '    p("Requirements and anathemas (anti-requirements) are interpreted as regular",',
+  '      "expressions, unless quoted.  An optional initial {#} or {#: myComment} deducts",',
+  '      "that many points for missing requirement or included anathemas.  (Negative values",',
+  '      "can be used for bonus points)"),',
+  '    textAreaInput("inputReq##", "Input requirements" ,width="720px", rows=6),',
+  '    textAreaInput("inputAnath##", "Input anathemas", width="720px", rows=6),',
+  '    textAreaInput("outputAnath##", "Output anathemas", width="720px", rows=6),',
+  '    textAreaInput("outputAnath##", "Output anathemas", width="720px", rows=6),',
+  '    HTML("&nbsp;"),',
+  '    p("An optional initial {#} or {#: myComment} is allowed for the following:"),',
+  '    textInput("minComments##", "Minimum number of comment lines", width="100%"),',
+  '    textInput("minBlanks##", "Minimum number of blank lines", width="100%"))'
 )
 
 
@@ -27,6 +49,9 @@ probPanelCode = lapply(1:problemCount,
                          txt = gsub("##", paste(index), probPanelCodeOne, fixed=TRUE)
                          return(paste(txt, collapse="\n"))
                        })
+
 probPanelCode = paste0("tabsetPanel(",
                        paste(probPanelCode, collapse=",\n"), "\n)")
+
+
 
