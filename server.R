344b0c26624955354bbd4c907c5a1edf6deb6e82
Howard Seltman <hseltman@stat.cmu.edu>
2018-09-10 13:10:06 -0400

    In server.R, suppress warnings on unzip() usually due to overwrite=FALSE.

--- server.R
+++ server.R
@@ -251,7 +251,7 @@ function(input, output, session) {
   
   observeEvent(input$unzip, {
     f = input$unzip$datapath
-    unzip(f, overwrite=FALSE, junkpaths=TRUE)
+    suppressWarnings(unzip(f, overwrite=FALSE, junkpaths=TRUE))
     allFiles(parseFileNames(list.files.only(), staticCanvasRE))
   })
   
